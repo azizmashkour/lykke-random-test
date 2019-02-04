024389f1fc30b8ed635b87a200c9d4c3c6faba04
azizmashkour <azizmashkour@gmail.com>
2019-02-04 19:40:34 +0100

    feat: import gallery component

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Gallery from 'react-grid-gallery';
+import images from '../common/gallery';
 
 const MainView = () => (
   <div className="Main-view">
     <h2>This is our Main View Component</h2>
+    <Gallery images={images} />
   </div>
 );
 
