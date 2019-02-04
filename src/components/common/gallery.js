cb26e9e0f372f57299d4056d6ad5db567cfbd962
azizmashkour <azizmashkour@gmail.com>
2019-02-04 19:40:19 +0100

    feat: add images gallery

---
+++ src/components/common/gallery.js
@@ -0,0 +1,31 @@
+// todo remove suppression on multiple exports
+// eslint-disable-next-line import/prefer-default-export
+const images = [{
+  src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
+  thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 174,
+  caption: 'After Rain (Jeshu John - designerspics.com)',
+  kind: 'forest',
+},
+{
+  src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
+  thumbnail: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 212,
+  tags: [
+    { value: 'Ocean', title: 'Ocean' },
+    { value: 'People', title: 'People' },
+  ],
+  caption: 'Boats (Jeshu John - designerspics.com)',
+  kind: 'lake',
+},
+{
+  src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+  thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 212,
+  kind: 'school',
+}];
+
+export default images;
