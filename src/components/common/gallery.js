1309bdb59f7687bef4ee028c62692db4f9bc1021
azizmashkour <azizmashkour@gmail.com>
2019-02-05 19:27:51 +0100

    chore: add more images to our gallery

--- src/components/common/gallery.js
+++ src/components/common/gallery.js
@@ -5,8 +5,11 @@ const images = [{
   thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
   thumbnailWidth: 320,
   thumbnailHeight: 174,
-  caption: 'After Rain (Jeshu John - designerspics.com)',
-  kind: 'forest',
+  tags: [
+    { value: 'Leaves', title: 'Leaves' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'leaves',
 },
 {
   src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
@@ -15,17 +18,98 @@ const images = [{
   thumbnailHeight: 212,
   tags: [
     { value: 'Ocean', title: 'Ocean' },
-    { value: 'People', title: 'People' },
   ],
-  caption: 'Boats (Jeshu John - designerspics.com)',
-  kind: 'lake',
+  caption: 'Picture by John Doe',
+  kind: 'ocean',
 },
 {
   src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
   thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
   thumbnailWidth: 320,
   thumbnailHeight: 212,
-  kind: 'school',
-}];
+  tags: [
+    { value: 'Pensils', title: 'Pencils' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'pensils',
+},
+{
+  src: 'https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg',
+  thumbnail: 'https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 212,
+  tags: [
+    { value: 'Building', title: 'Building' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'building',
+},
+{
+  src: 'https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg',
+  thumbnail: 'https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg',
+  thumbnailWidth: 620,
+  thumbnailHeight: 312,
+  tags: [
+    { value: 'Building', title: 'Building' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'building',
+},
+{
+  src: 'https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_n.jpg',
+  thumbnail: 'https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_n.jpg',
+  thumbnailWidth: 520,
+  thumbnailHeight: 212,
+  tags: [
+    { value: 'Forest', title: 'Forest' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'forest',
+},
+{
+  src: 'https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_n.jpg',
+  thumbnail: 'https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_n.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 252,
+  tags: [
+    { value: 'Forest', title: 'Forest' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'forest',
+},
+{
+  src: 'https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg',
+  thumbnail: 'https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 252,
+  tags: [
+    { value: 'Leaves', title: 'Leaves' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'leaves',
+},
+{
+  src: 'https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg',
+  thumbnail: 'https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 252,
+  tags: [
+    { value: 'Clock', title: 'Clok' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'clock',
+},
+{
+  src: 'https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg',
+  thumbnail: 'https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg',
+  thumbnailWidth: 400,
+  thumbnailHeight: 252,
+  tags: [
+    { value: 'Ship', title: 'Ship' },
+  ],
+  caption: 'Picture by John Doe',
+  kind: 'ship',
+},
+];
 
 export default images;
