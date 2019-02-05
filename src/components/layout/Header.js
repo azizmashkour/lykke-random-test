ada7ca27b7cc6671d8f092841775b74df3b2ba8a
azizmashkour <azizmashkour@gmail.com>
2019-02-05 19:28:33 +0100

    feat: add app usage message to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import './Header.css';
 
 const Header = () => (
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
+  <div>
+    <div className="header-title">
+      <span>
+        Discover random images with a filter option
+      </span>
+    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Filter images by related categories. Add&nbsp;
+        <code>?mode=forest</code>
+        &nbsp; in the URL params for forest images.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
 );
 
 export default Header;
